b835ab64fc807d9fc61df1dda5be0bf6407e5872
Craftyawesome <craftyawesome.ak@gmail.com>
2025-01-27 00:04:47 -0500

    fixes for 2.2.15

--- downloadassets.sh
+++ downloadassets.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-FULLURL="https://github.com/STJr/SRB2/releases/download/SRB2_release_2.2.14/SRB2-v2214-Full.zip"
+FULLURL="https://github.com/STJr/SRB2/releases/download/SRB2_release_2.2.15/SRB2-v2215-Full.zip"
 PATCHURL=""
 DOWNLOADCMD="curl -Lo"
 
